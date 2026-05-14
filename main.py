3b44297d8eef6cc83e51a6c9f688933e577217a1
lilsneq <bilmork2407@gmail.com>
2026-05-15 02:04:38 +0300

    добавил новый роутер

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ from handlers.rules import router as rules_router
 from handlers.to_back import router as back_router
 from handlers.my_question import router as my_question_router
 from handlers.change_question import router as change_question_router
+from handlers.change_find_question import router as change_find_question_router
 
 load_dotenv()
 
@@ -50,7 +51,10 @@ async def main() -> None:
     await CreateTableSQL().create_table()
 
 
-    dp.include_routers(registration_router, rules_router, my_question_router, change_question_router, back_router)
+    dp.include_routers(registration_router, rules_router,
+                       my_question_router, change_question_router,
+                       change_find_question_router, back_router,
+                       )
 
     bot = Bot(token=TOKEN_TG_BOT, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
