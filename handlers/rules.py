1fc6c55b27207e024d554b66dd892b0a8eece75b
lilsneq <bilmork2407@gmail.com>
2026-05-18 00:34:36 +0300

    добавил кнопку вернуться назад для регистрации

--- handlers/rules.py
+++ handlers/rules.py
@@ -5,8 +5,7 @@
 
 from aiogram import Router, F
 from aiogram.types import CallbackQuery, Message, InlineKeyboardMarkup
-from buttons.menu_button import back_to_main_menu_button
-
+from buttons.menu_button import back_to_main_menu_button, back_to_registration_menu_button
 
 # СКРИПТ
 
@@ -22,6 +21,15 @@ async def rules_handler(callback: CallbackQuery) -> None:
         text='ПРАВИЛ НЕТ',
         reply_markup=back_to_main_menu_button()
     )
+
+@router.callback_query(F.data == 'rules_btn_register')
+async def rules_register_handler(callback: CallbackQuery) -> None:
+
+    await callback.message.edit_text(
+        text='ПРАВИЛА СКОРО ПОЯВЯТСЯ',
+        reply_markup=back_to_registration_menu_button()
+    )
+
 
 
 
