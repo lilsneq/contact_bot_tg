7d3e40a2d096ce9d8ad00513a72074e1aefb1a92
lilsneq <bilmork2407@gmail.com>
2026-05-18 00:23:49 +0300

    изменил макс возраст

--- models_pydantic/pydantic_models.py
+++ models_pydantic/pydantic_models.py
@@ -14,7 +14,7 @@ class PydanticModelRegistration(BaseModel):
     """КЛАСС ФИЛЬТРАЦИЙ ПРИ РЕГИСТРАЦИИ"""
     username_id: int
     name: str = Field(min_length=1, max_length=255)
-    age: int = Field(ge=1, le=9999)
+    age: int = Field(ge=1, le=999)
     about: str | None = Field(default=None, max_length=255)
     image: str
     city: str = Field(min_length=1, max_length=255)
