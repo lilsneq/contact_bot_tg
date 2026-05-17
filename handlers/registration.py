431bfa5a8735b471d03df4ab5dd081726b9fe9c5
lilsneq <bilmork2407@gmail.com>
2026-05-18 00:33:26 +0300

    добавлены проверки на длину и текст от пользователя, и добавлен pydantic при отправки в базу данных

--- handlers/registration.py
+++ handlers/registration.py
@@ -2,6 +2,7 @@
 
 
 # ИМПОРТЫ
+import logging
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
@@ -13,6 +14,9 @@ from database.requests import CreateRequests
 
 from buttons.menu_button import get_registration_menu_button, get_main_menu_button, gender_button
 
+# pydantic
+from models_pydantic.pydantic_models import PydanticModelRegistration
+from pydantic import ValidationError
 
 # СКРИПТ
 
@@ -56,7 +60,6 @@ async def register_click_handlers(callback: CallbackQuery, state: FSMContext) ->
     """ХЕНДЛЕР РЕГИСТРАЦИИ ПОЛЬЗОВАТЕЛЯ"""
     await callback.answer()
 
-
     #регестрация в бд
     user_id = callback.from_user.id
     await CreateRequests.set_user_in_bd(user_id)
@@ -72,7 +75,11 @@ async def register_click_handlers(callback: CallbackQuery, state: FSMContext) ->
 async def register_name(message: Message, state: FSMContext) -> None:
     """хэндлер для того чтобы уловить имя"""
     if not message.text:
-        await message.answer('Ведите текст')
+        await message.answer('Введите текст')
+        return
+
+    if len(message.text) >= 255:
+        await message.answer('Текс слишком большой')
         return
 
     await state.update_data(name=message.text)
@@ -85,11 +92,13 @@ async def register_name(message: Message, state: FSMContext) -> None:
 @router.message(Registration.age)
 async def register_age(message: Message, state: FSMContext) -> None:
     """хэндрел для того чтобы уловить возраст"""
+
     if not message.text.isdigit():
         await message.answer('Пожалуйста введите возраст цифрами')
         return
 
     await state.update_data(age=int(message.text))
+    await state.update_data(age=message.text)
 
     await state.set_state(Registration.about)
 
@@ -99,11 +108,13 @@ async def register_age(message: Message, state: FSMContext) -> None:
 @router.message(Registration.about)
 async def register_about(message: Message, state: FSMContext) -> None:
     """хэндлер для того чтобы уловить о себе"""
+
     if not message.text:
-        await message.answer('Ведите текст')
+        await message.answer('Введите текст')
         return
+
     if len(message.text) >= 255:
-        await message.answer('О себе не должна занимать больше 255 символов')
+        await message.answer('Текс слишком большой')
         return
 
     await state.update_data(about=message.text)
@@ -116,6 +127,7 @@ async def register_about(message: Message, state: FSMContext) -> None:
 @router.message(Registration.image, F.photo)
 async def register_image(message: Message, state: FSMContext) -> None:
     """хэндлер для того чтобы уловить фото"""
+
     photo_id = message.photo[-1].file_id
     if not photo_id:
         await message.answer('Пришлите фото')
@@ -131,9 +143,16 @@ async def register_image(message: Message, state: FSMContext) -> None:
 
 @router.message(Registration.city)
 async def register_city(message: Message, state: FSMContext) -> None:
+    """хендлер для того чтобы уловить город"""
+
     if not message.text:
-        await message.answer('Ведите текст')
+        await message.answer('Введите текст')
         return
+
+    if len(message.text) >= 255:
+        await message.answer('Текс слишком большой')
+        return
+
 
     await state.update_data(city=message.text)
 
@@ -148,6 +167,7 @@ async def register_city(message: Message, state: FSMContext) -> None:
 
 @router.callback_query(Registration.gender)
 async def register_gender(callback: CallbackQuery, state: FSMContext) -> None:
+    """хендлер для того чтобы уловить гендер и отправить анкету в бд"""
     await callback.answer()
 
     if callback.data == 'gender_male':
@@ -160,20 +180,37 @@ async def register_gender(callback: CallbackQuery, state: FSMContext) -> None:
     await state.update_data(gender=gender_value)
 
 
-    ser_data = await state.get_data()
-    user_id = callback.from_user.id
+    user_data = await state.get_data()
 
-    await CreateRequests.set_question(
-        user_id=user_id,
-        name=ser_data['name'],
-        age=ser_data['age'],
-        about=ser_data['about'],
-        image=ser_data['image'],
-        city=ser_data['city'],
-        gender=ser_data['gender']
-    )
+    user_info_dict = {
+        'username_id': callback.from_user.id,
+        'name': user_data.get('name', 'нет имени'),
+        'age': user_data.get('age', 18),
+        'about': user_data.get('about', None),
+        'image': user_data.get('image', 'Нет фото'),
+        'city': user_data.get('city', 'нет города'),
+        'gender': user_data.get('gender', 'male')
+    }
 
-    await state.clear()
+    try:
+        validated_user = PydanticModelRegistration(**user_info_dict)
+        await CreateRequests.set_question(
+        user_id=validated_user.username_id,
+        name=validated_user.name,
+        age=validated_user.age,
+        about=validated_user.about,
+        image=validated_user.image,
+        city=validated_user.city,
+        gender=validated_user.gender
+        )
+        await state.clear()
+
+    except ValidationError as e:
+        logging.error(f"ОШИБКА pydantic В БАЗУ ДАННЫХ НИЧЕГО НЕ ДОБАВЛЕНО {e}", exc_info=True)
+
+        await callback.message.edit_text('Ошибка при заполнении анкеты, пожалуйста повторите попытку', reply_markup=get_registration_menu_button())
+        await state.clear()
+        return
 
     await callback.message.edit_text(
         text=' Вы успешно зарегистрировались!',
