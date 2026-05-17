b3309961e59cf249a8a724030f3c75c150bc1633
lilsneq <bilmork2407@gmail.com>
2026-05-18 00:33:38 +0300

    убрал None из about

--- models_pydantic/pydantic_models.py
+++ models_pydantic/pydantic_models.py
@@ -15,7 +15,7 @@ class PydanticModelRegistration(BaseModel):
     username_id: int
     name: str = Field(min_length=1, max_length=255)
     age: int = Field(ge=1, le=999)
-    about: str | None = Field(default=None, max_length=255)
+    about: str = Field(default=None, max_length=255)
     image: str
     city: str = Field(min_length=1, max_length=255)
     gender: str = Field(min_length=1, max_length=7)
