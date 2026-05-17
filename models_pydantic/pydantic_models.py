9b393cb956f3d78badb19162adaf742df57c204a
lilsneq <bilmork2407@gmail.com>
2026-05-17 22:22:43 +0300

    создан класс pydantic для фильтрации при регистрации

---
+++ models_pydantic/pydantic_models.py
@@ -0,0 +1,23 @@
+# МОДУЛЬ ДЛЯ ФИЛЬТРАЦИИ ЧЕРЕЗ pydantic
+
+# ИМПОРТЫ
+from pydantic import BaseModel, Field
+
+
+
+
+
+# СКРИПТ
+
+
+class PydanticModelRegistration(BaseModel):
+    """КЛАСС ФИЛЬТРАЦИЙ ПРИ РЕГИСТРАЦИИ"""
+    username_id: int
+    name: str = Field(min_length=1, max_length=255)
+    age: int = Field(ge=1, le=9999)
+    about: str | None = Field(default=None, max_length=255)
+    image: str
+    city: str = Field(min_length=1, max_length=255)
+    gender: str = Field(min_length=1, max_length=7)
+
+
